43afe649ce9f7b57c34a981a138b9f911432ebcf
Bryan <bryanlima200@outlook.com>
2020-12-13 22:20:09 -0300

    Add ex007 Exercício 007

---
+++ mundo-01/ex007.py
@@ -0,0 +1,5 @@
+# Desenvolva um programa qu leia as duas notas de aluno, calcule e mostre a sua média
+
+n1 = float(input('Informe a primeira nota: '))
+n2 = float(input('Informe a segunda nota: '))
+print('A média entre {:.1f} e {:.1f} é {:.1f}' .format(n1, n2, ((n1+n2)/2)))
