6f49dc8978be780ae93c1f389c9895d40532902b
Bryan <bryanlima200@outlook.com>
2021-01-03 17:56:39 -0300

    Add ex042 Exercício 042

---
+++ mundo-02/ex042.py
@@ -0,0 +1,22 @@
+# Refaça o DESAFIO 035 dos triângulos, acrescentando o recurso de mostrar que tipo de triângulo será formado:
+# - Equilátero: todos os lados iguais
+# - Isósceles: dois lados iguais
+# - Escaleno: todos os lados diferentes
+
+print('-=' * 12)
+print('Analisador de Triângulos')
+print('-=' * 12)
+n1 = float(input('Primeiro segmento: '))
+n2 = float(input('Segundo segmento: '))
+n3 = float(input('Terceiro segmento: '))
+
+if n1 < n2 + n3 and n2 < n1 + n3 and n3 < n1 + n2:
+    print('\nOs segmentos acima PODEM formar um triângulo ', end='')
+    if n1 == n2 == n3:
+        print('EQUILÁTERO')
+    elif n1 != n2 != n3 != n1:
+        print('ESCALENO')
+    else:
+        print('ISÓSCELES')
+else:
+    print('\nOs segmentos acima NÃO PODEM formar triângulo')
